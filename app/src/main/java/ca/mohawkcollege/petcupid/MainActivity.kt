71275366908f3a0a0f4202fda06d08755429339a
ChengkangHuang <haleyhuang0312@gmail.com>
2023-08-29 20:19:21 +0800

    [Update]Regular modified, remove some components and optimize MainActivity

--- app/src/main/java/ca/mohawkcollege/petcupid/MainActivity.kt
+++ app/src/main/java/ca/mohawkcollege/petcupid/MainActivity.kt
@@ -24,16 +24,12 @@ class MainActivity : AppCompatActivity() {
     private val TAG = "====MainActivity===="
     private lateinit var mAuth: FirebaseAuth
     private lateinit var currentUser: FirebaseUser
-    private lateinit var mediaUri: Uri
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main) // Inflate the layout
-        binding.lifecycleOwner = this
-
+        setContentView(R.layout.activity_main)
         mAuth = FirebaseAuth.getInstance()
-        initScreenOnClickHandler()
     }
 
     override fun onStart() {
@@ -51,6 +47,9 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    /**
+     * Register an activity result launcher for login activity
+     */
     private val myFirebaseAuthActivityResultLauncher = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()
     ) { result ->
@@ -61,71 +60,6 @@ class MainActivity : AppCompatActivity() {
         } else if (result.resultCode == LoginActivity.RESULT_CODE_FAILURE) {
             Log.d(TAG, "onActivityResult: Login failed")
             Snackbar.make(binding.root, "Login failed", Snackbar.LENGTH_SHORT).show()
-        }
-    }
-
-    private fun initScreenOnClickHandler() {
-        binding.profileImgBtn.setOnClickListener { if (mAuth.currentUser != null) mAuth.signOut() }
-        binding.camImgBtn.setOnClickListener { captureScenes(true) }
-        binding.camImgBtn.setOnLongClickListener { captureScenes(false); true }
-    }
-
-    /**
-     * Register an activity result launcher for taking pictures
-     */
-    private val myPictureActivityResultLauncher = registerForActivityResult(
-        ActivityResultContracts.TakePicture()
-    ) { success ->
-        if (success) {
-            Log.d(TAG, "onActivityResult: Picture capture SUCCESS")
-            Snackbar.make(binding.root, "Picture saved", Snackbar.LENGTH_SHORT).show()
-        } else {
-            Log.d(TAG, "onActivityResult: Picture capture CANCEL")
-            Snackbar.make(binding.root, "Picture capture cancelled", Snackbar.LENGTH_SHORT).show()
-        }
-    }
-
-    /**
-     * Register an activity result launcher for taking videos
-     */
-    private val myVideoActivityResultLauncher = registerForActivityResult(
-        ActivityResultContracts.CaptureVideo()
-    ) { success ->
-        if (success) {
-            Log.d(TAG, "onActivityResult: Video capture SUCCESS")
-            Snackbar.make(binding.root, "Video saved", Snackbar.LENGTH_SHORT).show()
-        } else {
-            Log.d(TAG, "onActivityResult: Video capture CANCEL")
-            Snackbar.make(binding.root, "Video capture cancelled", Snackbar.LENGTH_SHORT).show()
-        }
-    }
-
-    /**
-     * Capture scenes
-     * File name format: photo/video + _yyyyMMdd_HHmmss_ + .jpg/mp4
-     * @param isPicture: Boolean
-     * @return Unit
-     */
-    @SuppressLint("SimpleDateFormat")
-    private fun captureScenes(isPicture: Boolean) {
-        val timestamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-        val values = ContentValues().apply {
-            put(
-                MediaStore.Images.Media.DISPLAY_NAME,
-                if (isPicture) "photo_${timestamp}_.jpg" else "video_${timestamp}_.mp4")
-            put(
-                MediaStore.Images.Media.MIME_TYPE,
-                if (isPicture) "image/jpeg" else "video/mp4")
-            put(
-                MediaStore.Images.Media.RELATIVE_PATH,
-                if (isPicture) Environment.DIRECTORY_PICTURES else Environment.DIRECTORY_MOVIES)
-        }
-        mediaUri = if (isPicture) contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)!!
-        else contentResolver.insert(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, values)!!
-        Log.d(TAG, "saveMedia: uri: $mediaUri")
-        mediaUri.let {
-            if (isPicture) myPictureActivityResultLauncher.launch(it)
-            else myVideoActivityResultLauncher.launch(it)
         }
     }
 }
